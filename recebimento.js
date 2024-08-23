f7861cad4846702a44253c108a30f1ef9d009677
smspocoredondo <smspocoredondo@gmail.com>
2024-08-23 20:24:09 -0300

    Update recebimento.js
    Signed-off-by: smspocoredondo <smspocoredondo@gmail.com>

--- recebimento.js
+++ recebimento.js
@@ -101,7 +101,7 @@ function imprimirTabela(tabelaId) {
     const tabela = document.getElementById(tabelaId).outerHTML;
     const novaJanela = window.open('', '', 'height=600,width=800');
     novaJanela.document.write('<html><head><title>Imprimir Tabela</title>');
-    novaJanela.document.write('<style>body{font-family: Arial, sans-serif;} table{width: 100%; border-collapse: collapse;} table, th, td{border: 1px solid black; padding: 8px; text-align: left; fonte-size: 12px;} th{background-color: #f2f2f2;}</style>');
+    novaJanela.document.write('<style>body{font-family: Arial, sans-serif;} table{width: 100%; border-collapse: collapse;} table, th, td{border: 1px solid black; padding: 8px; text-align: left; font-size: 12px;} th{background-color: #f2f2f2;}</style>');
     novaJanela.document.write('</head><body>');
 
     // Adicionando uma imagem no topo da tabela
